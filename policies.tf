924fcf16b26e86f0e55a099a1ede955ea68c4ee1
Maxime Deveaux <dvxam123@gmail.com>
2024-02-29 14:54:20 +0000

    Add policies and vault_token

--- policies.tf
+++ policies.tf
@@ -1,8 +1,8 @@
-# # Create an admins policy in the test namespace
-# resource "vault_policy" "admin_policy" {
-#   name          = "admins"
-#   policy        = file("policies/admin-policy.hcl")
-# }
+# Create an admins policy in the test namespace
+resource "vault_policy" "admin_policy" {
+  name          = "admins"
+  policy        = file("policies/admin-policy.hcl")
+}
 
 # # # Create an admins policy in the admin/staging namespace
 # resource "vault_policy" "admin_policy_staging" {
@@ -20,20 +20,20 @@
 #   policy        = file("policies/admin-policy.hcl")
 # }
 
-# resource "vault_policy" "boundary-policy" {
-#   name = "boundary-policy"
-#   policy = file("policies/boundary-policy.hcl")
-# }
+resource "vault_policy" "boundary-policy" {
+  name = "boundary-policy"
+  policy = file("policies/boundary-policy.hcl")
+}
 
-# resource "vault_policy" "boundary-read" {
-#   name = "boundary-read"
-#   policy = file("policies/boundary-read.hcl")
-# }
+resource "vault_policy" "boundary-read" {
+  name = "boundary-read"
+  policy = file("policies/boundary-read.hcl")
+}
 
-# resource "vault_policy" "boundary-ssh" {
-#   name = "boundary-ssh"
-#   policy = file("policies/boundary-ssh.hcl")
-# }
+resource "vault_policy" "boundary-ssh" {
+  name = "boundary-ssh"
+  policy = file("policies/boundary-ssh.hcl")
+}
 
 # resource "vault_policy" "db" {
 #   name = "db-policy"
